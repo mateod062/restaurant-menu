e1ba6c1d544ca729e36b9372262f09a32e68868d
Bruno Bursić <brunobursic14@gmail.com>
2024-01-13 19:32:49 +0100

    Refractor dizajna

--- src/component/common/content/Content.js
+++ src/component/common/content/Content.js
@@ -1,8 +1,8 @@
 import "./Content.css"
 const Content = ({subtitle, children}) => {
   return (
-      <div className={"body"} >
-          <div className={"subtitle"}>
+      <div>
+          <div className={"subtitle text"}>
               <h1 className={"mt-3 mx-4"}><strong>{subtitle}</strong></h1>
           </div>
           {children}
--- src/component/navbar/NavBar.js
+++ src/component/navbar/NavBar.js
@@ -4,6 +4,40 @@ import "./NavBar.css"
 const NavBar = () => {
   return (
       <>
+        <nav className={"navbar bg-black"}>
+          <Link to={"/"} className={"fs-1 ps-4 pb-3 text-decoration-none title col"}>Restaurant Mini</Link>
+          <ul className={"navigation-menu list-group list-group-horizontal"}>
+            <li>
+              <Link to="/">Menu</Link>
+            </li>
+            <li>
+              <Link to="/meals">Meals</Link>
+            </li>
+            <li>
+              <Link to="/employees">Employees</Link>
+            </li>
+            <li>
+              <Link to="/info">Info</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Outlet/>
+      </>
+  )
+};
+
+export default NavBar;
+
+/*import {Link, Outlet} from "react-router-dom";
+import "./NavBar.css"
+
+const NavBar = () => {
+  return (
+      <>
         <nav className={"navigation"}>
           <Link to={"/"} className={"title"}>Restaurant Mini</Link>
           <div className={"navigation-menu"}>
@@ -32,4 +66,4 @@ const NavBar = () => {
   )
 };
 
-export default NavBar;
+export default NavBar;*/
